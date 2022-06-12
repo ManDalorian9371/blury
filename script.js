6f561a3d9298716771a5d5c0cf64833bfcf568a4
mostafa rafati <mostafarafati888@gmail.com>
2022-06-13 01:43:41 +0430

    add icon

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
-
+const loading = document.querySelector('.loading-block');
 let load = 0;
 let int = setInterval(bluring, 30);
 function bluring(){
@@ -13,6 +13,11 @@ function bluring(){
 	loadText.innerHTML = `${load}%`;
 	bg.style.filter = `blur(${30 - load * 30 / 100}px)`;
 	loadText.style.opacity = `${1 - load / 100}`;
+	loading.style.opacity = `${load / 100}`;
 
 	console.log(load);
 }
+loading.addEventListener('click', () => {
+	document.querySelector('.loading-block').style.display = 'none';
+	document.querySelector('#bgLoading').style.display = 'none';
+});
